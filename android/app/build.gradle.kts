628e70f9540fafcfec4210cdedccd2d2fd8dbe5d
Manikandan Bangaru <34433411+manikandan-bangaru@users.noreply.github.com>
2025-02-26 07:24:48 +0530

    updating ndk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.magiban.org.sudoku"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973" //flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
